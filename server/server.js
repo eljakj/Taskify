81b1342b0c3dffe3ebd5691192312b84b3203d9d
eljakj <jadjakhlab@gmail.com>
2026-04-14 00:09:30 +0200

    Update server file

--- server/server.js
+++ server/server.js
@@ -13,16 +13,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "https://taskify-azure-three.vercel.app/",
-    ],
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(express.json());
 
 if (!process.env.MONGODB_URI) {
